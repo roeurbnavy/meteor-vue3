0e1b1c26d77da3e29ea7037f43e769f7b5f262c6
Navy <roeurbnavy@gmail.com>
2023-02-13 16:05:27 +0700

    improve

--- imports/ui/Home.vue
+++ imports/ui/Home.vue
@@ -1,65 +1,15 @@
 <script setup>
 import { ref } from 'vue'
-import { subscribe, autorun } from 'vue-meteor-tracker'
-import { ContactCollection } from '../api/contact'
 
-subscribe('contacts')
-const list = autorun(() => ContactCollection.find({}).fetch()).result
-
+const list = ref([])
 const form = ref({
   name: 'dary',
   email: 'dary@gmail.com',
   imageUrl: 'dary.png',
 })
-const called = ref(0)
 
-const save = () => {
-  called.value++
-  Meteor.apply(
-    'contact.insert',
-
-    [{ ...form.value, called: called.value }],
-    {
-      throwStubExceptions: true,
-      returnStubValue: true,
-    },
-    (err, res) => {
-      console.log('error', err)
-      console.log('res', res)
-      called.value = 0
-    }
-  )
-
-  // console.log('start')
-  // for (let i = 0; i < 10; i++) {
-  //   Meteor.call('a', i, function (err, res) {
-  //     console.log('Result of a:', res)
-  //     Meteor.call('b', i, function (e, r) {
-  //       console.log('Result of b:', r)
-  //     })
-  //     console.log('After b is called. Have we seen the result of b yet?')
-  //   })
-  // }
-  // console.log('finish')
-
-  // console.log('start')
-  // for (let i = 0; i < 10; i++) {
-  //   Meteor.call('a', i, function (err, res) {
-  //     console.log('Result of a:', res)
-  //     Meteor.call('b', i, function (e, r) {
-  //       console.log('Result of b:', r)
-  //     })
-  //     console.log('After b is called. Have we seen the result of b yet?')
-  //   })
-  // }
-  // console.log('finish')
-}
-
-const remove = (id) => {
-  Meteor.call('contact.remove', { contactId: id }, (err) => {
-    console.log('err', err)
-  })
-}
+const save = () => {}
+const remove = () => {}
 </script>
 
 <template>
