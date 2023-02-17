0f1f6772bdae7d9425a67084cb8a518dd082a6c1
Navy <roeurbnavy@gmail.com>
2023-02-17 14:00:19 +0700

    remove file

--- imports/api/customers/methods.js
+++ imports/api/customers/methods.js
@@ -2,29 +2,12 @@ import { ValidatedMethod } from 'meteor/mdg:validated-method'
 import SimpleSchema from 'simpl-schema'
 import Customer from './customers'
 
-export const insertCustomer = new ValidatedMethod({
-  name: 'insertCustomer',
+export const insertCustomer1 = new ValidatedMethod({
+  name: 'insertCustomer1',
   mixins: [],
-  validate: Customer.schema.validator(),
-
-  run(doc) {
-    if (!Meteor.isServer) return false
-
-    try {
-      console.log('doc',doc);
-      // return Customer.insert(doc)
-    } catch (error) {
-      console.log('error',error);
-      throw new Meteor.Error('Insert customer error', error)
-    }
-  },
-})
-
-Meteor.methods({
-  insertCustomer1(doc){
-    // validate metehod
-    // Customer.schema.validate(doc)
-    new SimpleSchema({
+  validate:
+  // Customer.schema.validator(),
+  new SimpleSchema({
     name: String,
     telephone: {
       type: String,
@@ -35,16 +18,95 @@ Meteor.methods({
       optional: true,
     },
     status: String,
-  }).validate(doc)
+  }).validator(),
+  run(doc) {
+    if (!Meteor.isServer) return false
+
+    try {
+
+      // return Customer.insert(doc)
+    } catch (error) {
+      console.log('error', error)
+      throw new Meteor.Error('Insert customer error', error)
+    }
+  },
+})
+
+Meteor.methods({
+  findCustomers(selector) {
+    if (!Meteor.isServer) return false
+    selector = selector || {}
+
+    return Customer.find(selector).fetch()
+  },
+  insertCustomer(doc) {
+    // validate method
+    // Customer.schema.validate(doc)
+    new SimpleSchema({
+      name: String,
+      telephone: {
+        type: String,
+        optional: true,
+      },
+      address: {
+        type: String,
+        optional: true,
+      },
+      status: String,
+    }).validate(doc)
 
     if (!Meteor.isServer) return false
 
     try {
-      console.log('doc',doc);
+      console.log('doc', doc)
+      // Comsert1.insert(doc)
       return Customer.insert(doc)
     } catch (error) {
-      console.log('error',error);
+      console.log('error', error)
       throw new Meteor.Error('Insert customer error', error)
     }
-  }
+  },
+  updateCustomer(doc) {
+    // validate method
+    // Customer.schema.validate(doc)
+    new SimpleSchema({
+      _id: String,
+      name: String,
+      telephone: {
+        type: String,
+        optional: true,
+      },
+      address: {
+        type: String,
+        optional: true,
+      },
+      status: String,
+    }).validate(doc)
+
+    if (!Meteor.isServer) return false
+
+    try {
+      console.log('doc', doc)
+      return Customer.update({ _id: doc._id }, { $set: doc })
+    } catch (error) {
+      console.log('error', error)
+      throw new Meteor.Error('Update customer error', error)
+    }
+  },
+  removeCustomer(id) {
+    // validate method
+    // Customer.schema.validate(doc)
+    // new SimpleSchema({
+    //   id: String,
+    // }).validate(id)
+
+    if (!Meteor.isServer) return false
+
+    try {
+      return Customer.remove({ _id: id })
+    } catch (error) {
+      console.log('error', error)
+      throw new Meteor.Error('Remove customer error', error)
+    }
+  },
 })
--- imports/ui/CustomerForm.vue
+++ imports/ui/CustomerForm.vue
@@ -27,23 +27,81 @@
         </v-radio-group>
         <v-row>
           <v-col>
-            <v-btn color="success" class="mr-4" @click="validate"> Save </v-btn>
-            <v-btn color="error" @click="reset"> Cancel </v-btn>
+            <v-btn color="success" class="mr-4" @click="submit">
+              Save
+              <v-icon icon="fa-solid fa-floppy-disk"></v-icon>
+            </v-btn>
+            <v-btn color="error" @click="reset">
+              Cancel
+              <v-icon>
+                <i class="fa-solid fa-rotate-right"></i>
+              </v-icon>
+            </v-btn>
           </v-col>
         </v-row>
       </v-col>
     </v-row>
   </v-form>
+  <v-table class="mt-6">
+    <thead>
+      <tr>
+        <th class="text-left">Name</th>
+        <th class="text-left">Telephone</th>
+        <th class="text-left">Address</th>
+        <th class="text-left">Status</th>
+        <th class="text-left">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="it in list" :key="it.name">
+        <td>{{ it.name }}</td>
+        <td>{{ it.telephone }}</td>
+        <td>{{ it.address }}</td>
+        <td>{{ it.status }}</td>
+        <td>
+          <v-btn
+            color="success"
+            icon="fa-regular fa-pen-to-square"
+            size="x-small"
+            @click="edit(it)"
+            class="mr-4"
+          ></v-btn>
+          <v-btn
+            color="error"
+            icon="fa-solid fa-trash"
+            size="x-small"
+            @click="remove(it._id)"
+          ></v-btn>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
+<script>
+export default {
+  name: 'ComA',
+}
+</script>
+
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, watch, computed } from 'vue'
+
+// data properties
 const refForm = ref()
+const list = ref([])
 const form = ref({
   name: '',
   telephone: '',
   address: '',
+  // qty: 0,
+  // price: 0,
   status: 'active',
 })
+
+const num = ref(0)
+const str = ref('')
+const obj = ref({})
+
 const rules = {
   name: [
     (v) => !!v || 'Name is required',
@@ -51,22 +109,101 @@ const rules = {
   ],
 }
 
-const validate = async () => {
-  const { valid } = await refForm.value.validate()
+// method
+const fetchData = () => {
+  Meteor.call('findCustomers', (err, res) => {
+    if (err) {
+      console.log('error', err)
+    } else {
+      // this.list = res
+      list.value = res
+    }
+  })
+}
 
+const submit = async () => {
+  console.log('refForm', refForm.value)
+  const { valid } = await refForm.value.validate()
+  console.log('valid', valid)
   if (valid) {
-    Meteor.call('insertCustomer1', form.value, (err, res) => {
-      if (err) {
-        console.log(err)
-      } else {
-        reset()
-      }
-    })
+    if (form.value._id) {
+      update()
+    } else {
+      insert()
+    }
   }
+}
+
+const insert = () => {
+  console.log('insert', form.value)
+  // delete form.value.name
+  // form.value.name = ''
+  Meteor.call('insertCustomer', form.value, (err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      reset()
+      fetchData()
+    }
+  })
+}
+
+const edit = (row) => {
+  form.value = { ...row }
+}
+
+const update = () => {
+  console.log('update', form.value)
+  Meteor.call('updateCustomer', form.value, (err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      reset()
+      fetchData()
+    }
+  })
+}
+
+const remove = (id) => {
+  console.log(id)
+  Meteor.call('removeCustomer', id, (err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      fetchData()
+    }
+  })
 }
 
 const reset = () => {
   // refForm.value.resetValidation()
+  delete form.value._id
   refForm.value.reset()
+  form.value.status = 'active'
+  // form.value = {
+  //   name:'',
+  //   telephone:'',
+  //   address:'',
+  //   status:'active'
+  // }
 }
+
+// const status = ref('')
+// watch(
+//   () => form.value.name,
+//   (value) => {
+//     console.log('name change', value)
+//   }
+// )
+// watch(
+//   () => form.value.telephone,
+//   (value) => {
+//     console.log('telephone change', value)
+//   }
+// )
+
+// life cycle
+onMounted(() => {
+  fetchData()
+})
 </script>
