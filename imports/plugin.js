c934ffd5d749ae289e09e578142d43a434a4136f
Navy <roeurbnavy@gmail.com>
2023-02-23 12:01:14 +0700

    improve

--- imports/plugin.js
+++ imports/plugin.js
@@ -4,7 +4,7 @@ import { Quasar, AppFullscreen, Notify, Dialog, LoadingBar } from 'quasar'
 // Import icon libraries
 // import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
-import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+// import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
 
 // Import Quasar css
 import 'quasar/src/css/index.sass'
